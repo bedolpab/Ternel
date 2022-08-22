0ed089ae1f3735e985a6126140ba452eb8ba874b
pablo <bedol.pab@gmail.com>
2022-08-22 18:43:24 -0500

    Added Sum of Two Values

---
+++ SumOfTwoValues/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace SumOfTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 4, 5, 4, 1, -2, 5 };
+            Console.WriteLine(SumExists(numbers, 4));
+        }
+        static bool SumExists(int[] array, int value)
+        {
+            Hashtable values = new Hashtable();
+            for (int i = 0; i < array.Length; i++)
+            {
+                values.Add(i, array[i]);
+                if (values.ContainsValue(value - array[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
