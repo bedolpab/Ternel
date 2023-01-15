78bfaabc0536c86927b5a71519b6356296b41fdc
pablo <bedol.pab@gmail.com>
2023-01-14 20:04:46 -0600

    binary search in java

---
+++ BinarySearch/Program.java
@@ -0,0 +1,48 @@
+
+/*
+ * Binary Search implemented in java
+ */
+
+public class Program {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20 };
+    }
+
+    public static int BinarySearch(int[] array, int n, int key){
+        // Safety check
+        if(array == null | n == 0){
+            return -1;
+        }
+
+        int low = 0;
+        int high = array.length - 1;
+        int midpoint = low + (high-low)/2;
+        while(low < high){
+            midpoint = low + (high-low)/2;
+
+            // Early exit
+            if(key == array[low]){
+                return low;
+            }
+            else if(key == array[high]){
+                return high;
+            }
+            else if(key == array[midpoint]){
+                return midpoint;
+            }
+
+            // Search left half of array
+            if(key < array[midpoint]){
+                high = midpoint - 1;
+            }
+            // Search right half of array
+            else{
+                low = midpoint + 1;
+            }
+
+        }
+        return -1;
+    }
+
+
+}
